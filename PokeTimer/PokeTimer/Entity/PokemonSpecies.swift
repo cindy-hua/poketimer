c9f38dbe7127176b277f287d8912794ce4ff3b36
m.cindyhua@gmail.com <m.cindyhua@gmail.com>
2025-03-21 18:08:26 +0100

    delete old enum pokemon species

--- PokeTimer/PokeTimer/Entity/PokemonSpecies.swift
+++ PokeTimer/PokeTimer/Entity/PokemonSpecies.swift
@@ -7,26 +7,6 @@
 
 import Foundation
 
-//enum PokemonSpecies: String, Codable, CaseIterable {
-//    case pikachu, bulbasaur, charmander, squirtle, unknown
-//    
-//    /// Returns a human-readable name (e.g., "Bulbasaur" instead of "bulbasaur").
-//    var displayName: String {
-//        switch self {
-//        case .pikachu: return "Pikachu"
-//        case .bulbasaur: return "Bulbasaur"
-//        case .charmander: return "Charmander"
-//        case .squirtle: return "Squirtle"
-//        case .unknown: return "Unknown"
-//        }
-//    }
-//
-//    /// Returns the corresponding image name for each Pokémon species.
-//    var imageName: String {
-//        return self.rawValue
-//    }
-//}
-
 // Introduce the new PokemonSpecies struct
 struct PokemonSpecies: Identifiable, Codable, Hashable {
     let id: Int
